678d8f35117859f0f6103778c0cdcf65470e644a
sunny7657 <ksvetlana466@gmail.com>
2024-03-10 23:08:10 +0100

    Update 4.ts

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,21 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface PageProps {
+  title: string;
+}
+class Component<T> {
+  constructor(public props: T) {}
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<PageProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
 
-export {};
+const props: PageProps = { title: 'Interface 👌' };
+const page = new Page(props);
+page.pageInfo();
+
+export { };
